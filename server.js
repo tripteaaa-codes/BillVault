3ae1177255fe960f9513ed75d1aa80fb43c1000d
Tripti <triptitiwari495@gmail.com>
2026-08-17 21:48:04 +0530

    Connect BillVault to MongoDB

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
 dotenv.config();
 
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +20,8 @@ app.get("/", (req, res) => {
         message: "Welcome to BillVault API"
     });
 });
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
